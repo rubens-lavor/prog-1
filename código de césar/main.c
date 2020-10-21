e06c91aba5bef16cfc8a728978bed3df8a3dfa97
rubens-lavor <haryson07@gmail.com>
2020-10-21 18:02:53 -0300

    finalizado

--- código de césar/main.c
+++ código de césar/main.c
@@ -6,16 +6,31 @@ int main() {
 
     fgets(codigo, 400, stdin);
 
-    printf("%s", codigo);
+    //printf("%s", codigo);
 
+    int indice = 4;
     int i = 0;
+
+    int ver = 0;
+
     while (codigo[i] != '\0') {
+        if (codigo[i] > 'v') {
+            indice = indice - (122 - codigo[i]) - 1;
+            codigo[i] = 'a';
+        }
+
         if (codigo[i] == ' ') {
         } else {
-            codigo[i] = codigo[i] + 1;
-            printf("indice --> %d valor--> %c \n", i, codigo[i]);
+            codigo[i] = codigo[i] + indice;
+            //printf("i --> %d valor--> %c --> indice %d \n", i, codigo[i], indice);
         }
         i++;
+        indice = 4;
+    }
+
+    i = 0;
+    for (i; i < strlen(codigo); i++) {
+        codigo[i] = toupper(codigo[i]);
     }
 
     printf("%s", codigo);

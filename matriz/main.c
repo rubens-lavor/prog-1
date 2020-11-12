27537fa606354c9e692b92c7e35c72fc9f1dfa98
rubens-lavor <haryson07@gmail.com>
2020-11-12 19:58:39 -0300

    entrada com os espaços ok

--- matriz/main.c
+++ matriz/main.c
@@ -2,31 +2,32 @@
 
 int main() {
     int n = 0;
+    /*
     int r = 0;
 
     int vetor[20] = {};
     int voltou = 0;
 
+    */
     int i = 0;
     int j = 0;
 
     scanf("%d", &n);
 
-    for (i = 0; i < r; i++) {
-        scanf("%d", &vetor[i]);
+    int matriz[n][n];
+
+    for (i = 0; i < n; i++) {
+        //for (j = 1; j < n; j++) {
+            scanf("%d %d %d", &matriz[i][0], &matriz[i][1], &matriz[i][2]);
+        //}
     }
 
-    for (j = 1; j < n+1 ; j++) {
-        voltou = 0;
-        for (i = 0; i < r; i++) {
-            if (vetor[i] == j) {
-                voltou = 1;
-            }
-        }
 
-        if (!voltou) {
-            printf("%d\n", j);
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            printf("%d\n", matriz[i][j]);
         }
     }
+    
     return 0;
 }

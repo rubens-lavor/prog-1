40e64cdb425fb80f62abd7269dea54da04f7a8d6
rubens-lavor <haryson07@gmail.com>
2020-11-24 18:54:37 -0300

    fução retorna traço

--- 8 - operação com matrizes/biblio.c
+++ 8 - operação com matrizes/biblio.c
@@ -1,41 +1,38 @@
-#include <stdio.h>
 #include "biblio.h"
 
-void le_matriz(int A[][5],int x){
+#include <stdio.h>
+
+void le_matriz(int A[][5], int x) {
     //escreva o código da função aqui
     //analise o código do programa principal e veja como usar o return;
 }
 
-void imprime_matriz(int A[][5],int x){
+void imprime_matriz(int A[][5], int x) {
     //escreva o código da função aqui
     //analise o código do programa principal e veja como usar o return;
 }
 
-int traco(int A[][5]){
+int traco(int A[][5]) {
     int traco = 0;
     int i = 0;
-    for(i = 0 ; i< 5 ; i++)
-    {
- 
-        traco += matriz[i][i];
-        printf("\n"); //após cada i ser impressa, um salto de i
+    for (i = 0; i < 5; i++) {
+        traco += A[i][i];
     }
- 
-    printf("\nTraco da matriz: %d\n", traco);
+
+    return traco;
 }
 
-void maiores(int A[][5], int B[], int x){
+void maiores(int A[][5], int B[], int x) {
     //escreva o código da função aqui
     //analise o código do programa principal e veja como usar o return;
 }
 
-void imprime_vetor(int B[], int x){
+void imprime_vetor(int B[], int x) {
     //escreva o código da função aqui
     //analise o código do programa principal e veja como usar o return;
 }
 
-void simetrica(int A[][5], int x){
+void simetrica(int A[][5], int x) {
     //escreva o código da função aqui
     //analise o código do programa principal e veja como usar o return;
 }
-

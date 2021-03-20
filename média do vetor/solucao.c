2930c9d8fb01934f35edc579ca76e746efe21615
rubens-lavor <haryson07@gmail.com>
2021-03-20 18:28:01 -0300

    feat: definindo a função Scanf()

--- média do vetor/solucao.c
+++ média do vetor/solucao.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 
+void Scanf(char tipo[3],int *num){
+  int aux;
+  scanf(tipo,&aux);
+  *num = aux;
+  //printf("%i\n", num);
+}
+
 int main(){
-  int vet[] = {1,2,3,4,5,6,0,0,8,4,3}, num;
+  int vet[] = {1,2,3,4,5,6,0,0,8,4,3}, num=1;
   //leitura da quantidade de elementos do vetor necessaria
-  scanf("%i", &num);
+  Scanf("%i", &num);
 
   //codifique a partir daqui - não mexer na parte anterior
   //...
 
-  int somatorio = 0;
+  printf("%i\n", num);
+  float somatorio = 0.0;
   float media = 0.0;
   int i = 0;
   for(i;i<num;i++){
